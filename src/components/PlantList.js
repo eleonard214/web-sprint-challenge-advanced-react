77fa585ce459b072fb8a2352011cb327753e2e6c
Erica Leonard <elynn1979@live.com>
2021-05-16 21:51:04 -0400

    useForm Complete

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,7 +3,12 @@ import axios from "axios";
 
 export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
-
+  constructor(){
+    super();
+    this.state = {
+      plants: [],
+    };
+  }
   // when the component mounts:
   //   - fetch data from the server endpoint - http://localhost:3333/plants
   //   - set the returned plants array to this.state.plants
@@ -13,6 +18,10 @@ export default class PlantList extends Component {
     axios.get('http://localhost:3333/plants')
       .then(res => {
         console.log(res);
+        this.setState({
+          ...this.state,
+          plants: res.data,
+        })
       });
   }
 
